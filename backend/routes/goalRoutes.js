362d20a234dccf42bc38a406247a04ea514470c8
agrey tbamanya <agreytibamanya04@gmail.com>
2022-07-25 15:20:39 +0300

    some file have been modified

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+const { getGoals, setGoal, updateGoal, DeleteGoal } = require('../controllers/goalControllers')
+const { protect } = require('../middleware/authMiddleware')
+
+// routes for get and post 
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+
+// route for update anda delete 
+router.route('/:id').put(protect, updateGoal).delete(protect, DeleteGoal)
+
+module.exports = router
